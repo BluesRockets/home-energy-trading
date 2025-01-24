22665841d8e55744b8142248ef2f200b2d1ca464
Marcus <bluelmhai@gmail.com>
2025-01-24 15:24:27 -0500

    feat: new file

---
+++ src/dataprocessing/consumption/data_augmentation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def read_data():
+    file_path = '../../../data/input/power_grid_energy_consumption_dataset.xlsx'
+    sheet_name = 'UsageData'
+
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+    print(df.head())
+
+def get_sum(a, b):
+    return a + b
+
+
+def main():
+    read_data()
+
+
+if __name__ == "__main__":
+    main()
