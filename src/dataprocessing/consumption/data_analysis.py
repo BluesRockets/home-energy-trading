a937836721c3c4bb3b33fa2c4ad7be5f707d590a
Marcus <bluelmhai@gmail.com>
2025-01-29 14:36:50 -0500

    feat: consumption data analysis

---
+++ src/dataprocessing/consumption/data_analysis.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def read_data():
+    file_path = '../../../data/input/power_grid_energy_consumption_dataset.xlsx'
+    sheet_name = 'UsageData'
+
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+    return df
+
+def real_data_analysis():
+    df_real = pd.read_excel("../../../data/input/power_grid_energy_consumption_dataset.xlsx", sheet_name = 'UsageData')
+
+    print(df_real.describe())
+
+    sns.set_style("whitegrid")
+    fig, axes = plt.subplots(3, 1, figsize=(14, 12))
+
+    # daily cycle
+    df_real.groupby(df_real.index.hour).mean().plot(ax=axes[0], title="average daily load curve")
+
+    # weekly cycle
+    df_real.groupby([df_real.index.dayofweek, df_real.index.hour]).mean().unstack(0).plot(ax=axes[1], title="weekend and weekday difference")
+
+    # monthly cycle
+    df_real.resample("M").mean().plot(ax=axes[2], title="seasonal cycle")
+
+def draw_plot_by_day(df):
+    plt.figure(figsize=(10, 6))
+    for i in range(1, 10):
+        plt.plot(np.arange(24), df.iloc[i, 1:25].values, label=str(i) + "day (kWh)", marker='o', linestyle='-',
+                 linewidth=2)
+    plt.title("Energy Consumption Chart Per hours of a family", fontsize=16)
+    plt.xlabel("hours", fontsize=12)
+    plt.ylabel("consumption(kWh)", fontsize=12)
+    plt.xticks(np.arange(24), fontsize=10)
+    plt.grid(alpha=0.5)
+    plt.legend(fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
+def draw_plot_by_month(df):
+    plt.figure(figsize=(10, 6))
+    total_consumption = []
+    for i in range(0, 12):
+        consumption_daily = 0
+        for item in df.iloc[30 * i, 1:25].values:
+            consumption_daily += item
+        total_consumption.append(consumption_daily)
+    print(total_consumption)
+    plt.plot(np.arange(12), total_consumption, label=str(i) + "day (kWh)", marker='o', linestyle='-',
+                 linewidth=2)
+    plt.title("Energy Consumption Chart a day in 12 months", fontsize=16)
+    plt.xlabel("month", fontsize=12)
+    plt.ylabel("consumption(kWh)", fontsize=12)
+    plt.xticks(np.arange(12), fontsize=10)
+    plt.grid(alpha=0.5)
+    plt.legend(fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
+
+def main():
+    df = read_data()
+    # draw_plot_by_day(df)
+    # draw_plot_by_month(df)
+    real_data_analysis()
+
+
+
+
+if __name__ == "__main__":
+    main()
--- src/dataprocessing/consumption/data_augmentation.py
+++ src/dataprocessing/consumption/data_augmentation.py
@@ -1,4 +1,9 @@
+import numpy as np
 import pandas as pd
+from scipy.stats import skewnorm
+import matplotlib.pyplot as plt
+
+NUMBER_OF_FAMILIES = 1
 
 def read_data():
     file_path = '../../../data/input/power_grid_energy_consumption_dataset.xlsx'
@@ -6,10 +11,71 @@ def read_data():
 
     df = pd.read_excel(file_path, sheet_name=sheet_name)
 
-    print(df.head())
+    return df
+
+def get_consumption_level_factor():
+    num = np.random.randint(1, 100)
+    if num <= 20:
+        return np.random.randint(6, 9) / 10
+    elif num <=60:
+        return np.random.randint(9, 11) / 10
+    else:
+        return np.random.randint(11, 18) / 10
+
+def get_family_member_factor():
+    num = np.random.randint(1, 100)
+    if num <= 20:
+        return np.random.randint(6, 9) / 10
+    elif num <=60:
+        return np.random.randint(9, 11) / 10
+    else:
+        return np.random.randint(11, 18) / 10
+
+
+def get_season_factor(hour):
+
+    return
+
+
+def generate_household(hh_id, year=2024):
+    index = pd.date_range(f"{year}-01-01", f"{year}-12-31 23:00", freq="h")
+    df = pd.DataFrame(index=index)
+    df["hour"] = df.index.hour
+    df["day_of_week"] = df.index.dayofweek
+    df["is_weekend"] = df.day_of_week >= 5
+    df["season"] = df.index.month.map({12: 0, 1: 0, 2: 0, 3: 1, 4: 1, 5: 1, 6: 2, 7: 2, 8: 2, 9: 3, 10: 3, 11: 3})
+
+    # household config
+    params = {
+        "base_load": 1,
+        "consumption_level": get_consumption_level_factor(),
+        "family_member": get_family_member_factor,
+        "weekend_multiplier": 1 + np.random.uniform(-1, 1) * 0.3,
+        "summer_boost": np.random.uniform(1.2, 1.8) if np.random.rand() < 0.7 else 1.0,
+    }
+
+    df["load"] = params["base_load"]
+
+    df["load"] += np.sin(df["hour"] / 24 * 2 * np.pi) * 0.5 + 0.5
+
+    # weekend boost
+    df["load"] *= np.where(df["is_weekend"], params["weekend_multiplier"], 1)
+
+    # TODO daily cycle
+
+    # TODO seasonal fluctuation
+
+
+    # add noise
+    noise = skewnorm.rvs(5, loc=0, scale=0.1, size=len(df))
+    df["load"] = np.abs(df["load"] + noise)
+
+    return df
+
 
 def main():
-    read_data()
+    for i in range(NUMBER_OF_FAMILIES):
+        generate_household(i)[["load"]]
 
 
 if __name__ == "__main__":
